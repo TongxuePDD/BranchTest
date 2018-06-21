2a68287d12821c1e0f3956a32eb8a439e053657f
TongxuePDD <40015597+TongxuePDD@users.noreply.github.com>
2018-06-21 17:04:09 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import math
+a = 0.8224
+b = math.pow(931,0.3)
+print(a/b) 
+
+a  = 	0.6784
+b = math.pow(490,0.3) 
+print(a/b) 
+c = a/b
+print( math.sqrt(c * math.pow(490,0.3)) )
+
+
+d = math.sqrt(0.8224)*10000
+e  = math.sqrt(0.6784)*10000
+
+print(d)
+print(e)
